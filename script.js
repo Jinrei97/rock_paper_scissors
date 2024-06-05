ae8e1310e32fb1976d5543fd92a3987d125c8348
Jinrei97 <121897304+Jinrei97@users.noreply.github.com>
2024-06-05 19:03:40 +0200

    Inizio aggiunta ui:
    - aggiunti pulsanti che richiamano "playRound" con
     la relativa scelta;
    - aggiunto div che mostra il risultato;
    - eliminata la funzione "getPlayerChoice", ora la
     scelta viene fatta tramite i pulsanti.

--- script.js
+++ script.js
@@ -8,88 +8,63 @@ function getComputerChoice() {
         return "scissors";
     }
 };
-/*
-function test(n){
-    let n_rock = 0;
-    let n_paper = 0;
-    let n_scissors = 0;
-    for (let i = 0; i <= n; i++) {
-       let choice = getComputerChoice();
-       switch (choice) {
-        case "rock":
-            n_rock += 1;
-            break;
-        case "paper":
-            n_paper += 1;
-            break;
-        case "scissors":
-            n_scissors += 1;
-       }
-    }
-    console.log(`rocks: ${n_rock}, papers: ${n_paper}, scissors ${n_scissors}`)
-};
-*/
 
-function getHumanChoice() {
-    let choice = prompt("rock, paper or scissors?");
-    choice = choice.toLowerCase();
-    switch (choice) {
-        case "rock":
-        case "paper":
-        case "scissors":
-            return choice;
-            break;
-        default:
-            alert("scelta non valida");
-            return getHumanChoice();
+let computerScore = 0;
+let humanScore = 0;
+
+const buttons = document.querySelectorAll("div button");
+
+buttons.forEach((button) => {
+   button.addEventListener("click", e => {
+    playRound(e.target.textContent, getComputerChoice()); 
+   });
+});
+
+function playRound(human = "rock", cpu = getComputerChoice()) {
+    const result_div = document.querySelector(".result");
+    human = human.toLowerCase();
+    if (human === cpu){
+        result_div.textContent = "it's a tie!";
     }
+    switch (human) {
+        case "rock":
+            if (cpu === "paper") {
+                result_div.textContent = "You lose! paper beats rock.";
+                computerScore += 1;
+            } else {
+                result_div.textContent = "You win! rock beats scissors";
+                humanScore += 1;
+            }
+            break;
+
+        case "paper":
+            if (cpu === "scissors") {
+                result_div.textContent = "You lose! scissors beats paper.";
+                computerScore += 1;
+            } else {
+                result_div.textContent = "You win! paper beats rock";
+                humanScore += 1;
+            }
+            break;
+
+        case "scissors":
+            if (cpu === "rock") {
+                result_div.textContent = "You lose! rock beats scissors.";
+                computerScore += 1;
+            } else {
+                result_div.textContent = "You win! scissors beats paper";
+                humanScore += 1;
+            }
+    }    
+    console.log(`score -- human ${humanScore} | cpu ${computerScore}`);
 }
 
 
-
+/*
 function playGame() {
-    let computerScore = 0;
-    let humanScore = 0;
-
-    function playRound(human = getHumanChoice(), cpu = getComputerChoice()) {
-        if (human === cpu){
-            console.log("it's a tie!");
-        }
-        switch (human) {
-            case "rock":
-                if (cpu === "paper") {
-                    console.log("You lose! paper beats rock.")
-                    computerScore += 1;
-                } else {
-                    console.log("You win! rock beats scissors");
-                    humanScore += 1;
-                }
-                break;
-
-            case "paper":
-                if (cpu === "scissors") {
-                    console.log("You lose! scissors beats paper.")
-                    computerScore += 1;
-                } else {
-                    console.log("You win! paper beats rock");
-                    humanScore += 1;
-                }
-                break;
-
-            case "scissors":
-                if (cpu === "rock") {
-                    console.log("You lose! rock beats scissors.")
-                    computerScore += 1;
-                } else {
-                    console.log("You win! scissors beats paper");
-                    humanScore += 1;
-                }
-        }    
-        console.log(`score -- human ${humanScore} | cpu ${computerScore}`);
-    }
-
     for (let i = 0; i < 5; i++){
         playRound();
     }
     (humanScore > computerScore) ? console.log("You win!") : console.log("You lose!");
 }
+*/
